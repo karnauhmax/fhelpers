9cd50f8bfb0989b0c5392c7104860b4014e3c2e1
Maksym Karnaukh <maksym.karnaukh@jatapp.com>
2025-12-16 19:20:47 +0100

    basic tw setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  css: ["~/assets/css/main.css"],
 
   imports: {
     autoImport: false,
@@ -9,5 +13,9 @@ export default defineNuxtConfig({
 
   components: {
     dirs: [],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
